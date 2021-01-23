1888d25c26dcc849e107072cde77256ea03a2250
hendraaagil <hendraaagil@gmail.com>
2021-01-23 21:09:56 +0700

    Add Create Function

--- 4 CRUD With SQL Server/Form1.Designer.cs
+++ 4 CRUD With SQL Server/Form1.Designer.cs
@@ -29,22 +29,167 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.tbSatuan = new System.Windows.Forms.TextBox();
+            this.tbJumlah = new System.Windows.Forms.TextBox();
+            this.tbHrgJual = new System.Windows.Forms.TextBox();
+            this.tbHrgBeli = new System.Windows.Forms.TextBox();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbKodeBarang = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 190);
+            this.dataGridView1.Location = new System.Drawing.Point(21, 180);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(719, 195);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Size = new System.Drawing.Size(665, 195);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimpan.Location = new System.Drawing.Point(102, 135);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(92, 32);
+            this.btnSimpan.TabIndex = 7;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // tbSatuan
+            // 
+            this.tbSatuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSatuan.Location = new System.Drawing.Point(235, 100);
+            this.tbSatuan.Name = "tbSatuan";
+            this.tbSatuan.Size = new System.Drawing.Size(129, 20);
+            this.tbSatuan.TabIndex = 6;
+            // 
+            // tbJumlah
+            // 
+            this.tbJumlah.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbJumlah.Location = new System.Drawing.Point(102, 100);
+            this.tbJumlah.Name = "tbJumlah";
+            this.tbJumlah.Size = new System.Drawing.Size(65, 20);
+            this.tbJumlah.TabIndex = 5;
+            // 
+            // tbHrgJual
+            // 
+            this.tbHrgJual.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbHrgJual.Location = new System.Drawing.Point(272, 74);
+            this.tbHrgJual.Name = "tbHrgJual";
+            this.tbHrgJual.Size = new System.Drawing.Size(92, 20);
+            this.tbHrgJual.TabIndex = 4;
+            // 
+            // tbHrgBeli
+            // 
+            this.tbHrgBeli.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbHrgBeli.Location = new System.Drawing.Point(102, 74);
+            this.tbHrgBeli.Name = "tbHrgBeli";
+            this.tbHrgBeli.Size = new System.Drawing.Size(92, 20);
+            this.tbHrgBeli.TabIndex = 3;
+            // 
+            // tbNama
+            // 
+            this.tbNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNama.Location = new System.Drawing.Point(102, 48);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(262, 20);
+            this.tbNama.TabIndex = 2;
+            // 
+            // tbKodeBarang
+            // 
+            this.tbKodeBarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbKodeBarang.Location = new System.Drawing.Point(102, 22);
+            this.tbKodeBarang.Name = "tbKodeBarang";
+            this.tbKodeBarang.Size = new System.Drawing.Size(127, 20);
+            this.tbKodeBarang.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(188, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Satuan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Jumlah";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(208, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Harga Jual";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Harga Beli";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nama";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Kode Barang";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(743, 397);
+            this.ClientSize = new System.Drawing.Size(711, 397);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.tbSatuan);
+            this.Controls.Add(this.tbJumlah);
+            this.Controls.Add(this.tbHrgJual);
+            this.Controls.Add(this.tbHrgBeli);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.tbKodeBarang);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -52,12 +197,26 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.TextBox tbSatuan;
+        private System.Windows.Forms.TextBox tbJumlah;
+        private System.Windows.Forms.TextBox tbHrgJual;
+        private System.Windows.Forms.TextBox tbHrgBeli;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbKodeBarang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- 4 CRUD With SQL Server/Form1.cs
+++ 4 CRUD With SQL Server/Form1.cs
@@ -24,7 +24,17 @@ namespace _4_CRUD_With_SQL_Server
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void clear()
+        {
+            tbKodeBarang.Clear();
+            tbNama.Clear();
+            tbHrgBeli.Text = "0";
+            tbHrgJual.Text = "0";
+            tbJumlah.Text = "0";
+            tbSatuan.Clear();
+        }
+
+        private void showData()
         {
             SqlConnection conn = connection.GetConn();
             try
@@ -46,6 +56,46 @@ namespace _4_CRUD_With_SQL_Server
             {
                 conn.Close();
             }
+        }
+
+        private void createData()
+        {
+            if (tbKodeBarang.Text.Trim() == "" || tbNama.Text.Trim() == "" || tbHrgBeli.Text.Trim() == "" || tbHrgJual.Text.Trim() == "" || tbJumlah.Text.Trim() == "" || tbSatuan.Text.Trim() == "")
+            {
+                MessageBox.Show("Data Harus Lengkap!");
+            }
+            else
+            {
+                SqlConnection conn = connection.GetConn();
+                try
+                {
+                    conn.Open();
+                    sc = new SqlCommand("INSERT INTO TBL_BARANG VALUES('" + tbKodeBarang.Text + "', '" + tbNama.Text + "', '" + tbHrgBeli.Text + "', '" + tbHrgJual.Text + "', '" + tbJumlah.Text + "', '" + tbSatuan.Text + "')", conn);
+                    sc.ExecuteNonQuery();
+                    MessageBox.Show("Data Berhasil Ditambahkan!");
+                    showData();
+                    clear();
+                }
+                catch (Exception E)
+                {
+                    MessageBox.Show(E.ToString());
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showData();
+            clear();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            createData();
         }
     }
 }
