ff59045c116aea2284477167d37bdbc13620ab52
hendraaagil <hendraaagil@gmail.com>
2021-01-28 21:17:21 +0700

    Change StartUp Form

---
+++ 4 CRUD With SQL Server/FormTransaksiJual.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_CRUD_With_SQL_Server
+{
+    public partial class FormTransaksiJual : Form
+    {
+        public FormTransaksiJual()
+        {
+            InitializeComponent();
+        }
+    }
+}
